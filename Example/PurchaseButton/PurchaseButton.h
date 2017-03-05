dcf8410187e3c0e622105073f62d7472e28dd32e
Jon Andersen <jon@andersen.re>
2017-03-05 14:01:29 -0500

    Support for carthage

---
+++ Example/PurchaseButton/PurchaseButton.h
@@ -0,0 +1,20 @@
+//
+//  PurchaseButton.h
+//  PurchaseButton
+//
+//  Created by Jon Andersen on 3/5/17.
+//  Copyright © 2017 AveApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PurchaseButton.
+FOUNDATION_EXPORT double PurchaseButtonVersionNumber;
+
+//! Project version string for PurchaseButton.
+FOUNDATION_EXPORT const unsigned char PurchaseButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurchaseButton/PublicHeader.h>
+
+
+#import <PurchaseButton/AvePurchaseButton.h>
